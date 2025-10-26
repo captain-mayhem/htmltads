4ec75eafa7fab9d18e697be92bbfc1730396f55b
Captain <captain@captain-online.de>
2025-10-26 22:14:22 +0100

    Made docsearch shared library.

--- textindex/docsearch.h
+++ textindex/docsearch.h
@@ -1,25 +1,27 @@
 #ifndef DOCSEARCH_H
 #define DOCSEARCH_H
 
-int docsearch_available();
+#include <docsearch_export.h>
 
-struct docsearch_result_list_t {
+DOCSEARCH_EXPORT int docsearch_available();
+
+struct DOCSEARCH_EXPORT docsearch_result_list_t {
 
 };
 
 typedef struct docsearch_result_list_t* docsearch_result_list;
 
-struct docsearch_details {
+struct DOCSEARCH_EXPORT docsearch_details {
     const char* fname;
     const char* title;
     const char* summary;
 };
 
-docsearch_result_list docsearch_execute(int x, const char* query, int* count, const char* indexfile,
+DOCSEARCH_EXPORT docsearch_result_list docsearch_execute(int x, const char* query, int* count, const char* indexfile,
     const char* dir, const char* w32_doc_dirs[], int i);
 
-int docsearch_get_details(docsearch_result_list lst, int i, docsearch_details* d);
+DOCSEARCH_EXPORT int docsearch_get_details(docsearch_result_list lst, int i, docsearch_details* d);
 
-void docsearch_free_list(docsearch_result_list lst);
+DOCSEARCH_EXPORT void docsearch_free_list(docsearch_result_list lst);
 
 #endif
